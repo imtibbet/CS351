878365d9ae391ee8184e6c1c87516efd9a54ec1f
imtibbet <tibbian@gmail.com>
2014-09-15 20:30:34 -0400

    initial attempt for image function implementations

--- graphics/lib/images.c
+++ graphics/lib/images.c
@@ -21,6 +21,33 @@ The images structure function implementations
  * structure. Returns a NULL pointer if the operation fails.
  */
 Image *image_create(int rows, int cols){
+
+	int i;
+	Image *src = NULL;
+
+	// use malloc to reserve memory for image, returning NULL on failure
+	src = (Image *)malloc(sizeof(Image)); 
+	if(!src){
+		return(NULL);
+	}
+	src->data = (FPixel **)malloc(sizeof(FPixel *) * rows); 
+	if(!src->data){
+		return(NULL);
+	}
+	src->data[0] = (FPixel *)malloc(sizeof(FPixel) * rows * cols);
+	if(!src->data[0]){
+		return(NULL);
+	}
+
+	// assign all row pointers
+	for(i=1;i<rows-1;i++){
+		src->data[i] = &(src->data[0][i * cols]);
+	}
+
+	// assign image data fields to parameter and return pointer to image
+	src->rows = rows;
+	src->cols = cols;
+	return(src);
 	
 } // end image_create
 
@@ -29,6 +56,8 @@ Image *image_create(int rows, int cols){
  */
 void image_free(Image *src){
 
+	free(src);
+
 } // end image_free
 
 /*
@@ -36,6 +65,9 @@ void image_free(Image *src){
  * to zero and the data field to NULL.
  */
 void image_init(Image *src){
+
+	src->data = NULL;
+	src->rows = src->cols = 0;
 
 } // end image_init
 
@@ -48,6 +80,34 @@ void image_init(Image *src){
  */
 int image_alloc(Image *src, int rows, int cols){
 
+	int i, j;
+	
+	// free existing memory if rows and cols are both non-zero.
+	if(src->rows != 0 and src->cols != 0){
+		free(src->data);
+	}
+
+	// malloc new memory spave for data and return non-zero on failure
+	src->data = (FPixel **)malloc(sizeof(FPixel *) * rows); 
+	if(!src->data){return(1);}
+	src->data[0] = (FPixel *)malloc(sizeof(FPixel) * rows * cols);
+	if(!src->data[0]){return(1);}
+
+	// assign all row pointers
+	for(i=0;i<rows;i++){
+		src->data[i] = &(src->data[0][i * cols]);
+
+		// initialize data to appropriate values
+		for(j=0;j<cols;i++){
+			src->data[i][j].rgb[0] = 
+			src->data[i][j].rgb[1] = 
+			src->data[i][j].rgb[2] = 
+			src->data[i][j].a = 0.0;
+			src->data[i][j].z = 1.0;
+		}
+	}
+	return(0);
+	
 } // end image_alloc
 
 /*
@@ -55,6 +115,10 @@ int image_alloc(Image *src, int rows, int cols){
  * The function does not free the Image structure.
  */
 void image_dealloc(Image *src){
+
+	free(src->data[0])
+	free(src->data)
+	src->rows = src->cols = 0;
 
 } // end image_dealloc
 
@@ -69,6 +133,23 @@ void image_dealloc(Image *src){
  */
 Image *image_read(char *filename){
 
+	Image *src;
+	Pixel *readData;
+	int i, rows, cols, colors;
+
+	readData = readPPM(&rows, &cols, &colors, filename);
+	if(!readData){
+		return(NULL);
+	}
+	src = image_create(rows, cols);
+	for(i=0;i<rows*cols;i++){
+		src->data[0][i].rgb[0] = (float)readData[i].r;
+		src->data[0][i].rgb[1] = (float)readData[i].g;
+		src->data[0][i].rgb[2] = (float)readData[i].b;
+		src->data[0][i].a = src->data[0][i].z = 1.0;
+	}
+	return(src);
+	
 } // end image_read
 
 /*
@@ -78,6 +159,9 @@ Image *image_read(char *filename){
  */
 int image_write(Image *src, char *filename){
 
+	writePPM(src, src->rows, src->cols, 100 /* s/b 255 */, filename);
+	return(0);
+
 } // end image_write
 
 // Access
@@ -86,41 +170,42 @@ int image_write(Image *src, char *filename){
  * returns the FPixel at (r, c).
  */
 FPixel image_getf(Image *src, int r, int c){
-
+	return(src->data[r][c]);
 } // end image_getf
 
 /*
  * returns the value of band b at pixel (r, c).
  */
 float image_getc(Image *src, int r, int c, int b){
-
+	return(src->data[r][c][b]);
 } // end image_getf
 
 /*
  * returns the alpha value at pixel (r, c).
  */
 float image_geta(Image *src, int r, int c){
-
+	return(src->data[r][c].a);
 } // end image_geta
 
 /*
  * returns the depth value at pixel (r, c).
  */
 float image_getz(Image *src, int r, int c){
-
+	return(src->data[r][c].z);
 } // end image_getz
 
 /*
  * sets the values of pixel (r, c) to the FPixel val.
  */
 void image_setf(Image *src, int r, int c, FPixel val){
-
+	src->data[r][c] = val;
 } // end image_setf
 
 /*
  * sets the value of pixel (r, c) band b to val.
  */
 void image_setc(Image *src, int r, int c, int b, float val){
+	src->data[r][c][b] = val;
 
 } // end image_setc
 
@@ -128,14 +213,14 @@ void image_setc(Image *src, int r, int c, int b, float val){
  * sets the alpha value of pixel (r, c) to val.
  */
 void image_seta(Image *src, int r, int c, float val){
-
+	src->data[r][c].a = val;
 } // end image_seta
 
 /*
  * sets the depth value of pixel (r, c) to val.
  */
 void image_setz(Image *src, int r, int c, float val){
-
+	src->data[r][c].z = val;
 } // end image_setz
 
 // Utility
@@ -145,218 +230,67 @@ void image_setz(Image *src, int r, int c, float val){
  * z value of 1.0).
  */
 void image_reset(Image *src){
-
+	int i, j;
+	for(i=0;i<src->rows;i++){
+		for(j=0;j<src->cols;j++){
+			src->data[i][j].rgb[0] = 
+			src->data[i][j].rgb[1] = 
+			src->data[i][j].rgb[2] = 0.0;
+			src->data[i][j].a = 
+			src->data[i][j].z = 1.0;
+		}
+	}
 } // end image_reset
 
 /*
  * sets every FPixel to the given value.
  */
 void image_fill(Image *src, FPixel val){
-
+	int i, j;
+	for(i=0;i<src->rows;i++){
+		for(j=0;j<src->cols;j++){
+			src->data[i][j].rgb[0] = val.r;
+			src->data[i][j].rgb[1] = val.g;
+			src->data[i][j].rgb[2] = val.b;
+		}
+	}
 } // end image_fill
 
 /*
  * sets the (r, g, b) values of each pixel to the given color.
  */
 void image_fillrgb(Image *src, float r, float g, float b){
-
+	int i, j;
+	for(i=0;i<src->rows;i++){
+		for(j=0;j<src->cols;j++){
+			src->data[i][j].rgb[0] = r;
+			src->data[i][j].rgb[1] = g;
+			src->data[i][j].rgb[2] = b;
+		}
+	}
 } // end image_fillrgb
 
 /*
  * sets the alpha value of each pixel to the given value.
  */
 void image_filla(Image *src, float a){
-
+	int i, j;
+	for(i=0;i<src->rows;i++){
+		for(j=0;j<src->cols;j++){
+			src->data[i][j].a = a;
+		}
+	}
 } // end image_filla
 
 /*
  * sets the z value of each pixel to the given value.
  */
 void image_fillz(Image *src, float z){
-
+	int i, j;
+	for(i=0;i<src->rows;i++){
+		for(j=0;j<src->cols;j++){
+			src->data[i][j].z = z;
+		}
+	}
 } // end image_fillz
-
-////////////////////////////////////////////////////////////////////////////////
-
-// read in rgb values from the ppm file output by cqcam
-Pixel *readPPM(int *rows, int *cols, int * colors, char *filename) {
-   char tag[40];
-   Pixel *image;
-   FILE *fp;
-   int read, num[3], curchar;
-
-   if(filename != NULL && strlen(filename))
-     fp = fopen(filename, "r");
-   else
-     fp = stdin;
-
-   if(fp) {
-     fscanf(fp, "%s\n", tag);
-
-     // Read the "magic number" at the beginning of the ppm
-     if (strncmp(tag, "P6", 40) != 0) {
-       fprintf(stderr, "not a ppm!\n");
-       exit(1);
-     }
-     
-     // Read the rows, columns, and color depth output by cqcam
-     // need to read in three numbers and skip any lines that start with a #
-     read = 0;
-     while(read < 3) {
-       curchar = fgetc(fp);
-       if((char)curchar == '#') { // skip this line
-	 while(fgetc(fp) != '\n')
-	   /* do nothing */;
-       }
-       else {
-	 ungetc(curchar, fp);
-	 fscanf(fp, "%d", &(num[read]));
-	 read++;
-       }
-     }
-     while(fgetc(fp) != '\n')
-       /* pass the last newline character */;
-
-     *cols = num[0];
-     *rows = num[1];
-     *colors = num[2];
-
-     if(*cols > 0 && *rows > 0) {
-#if USECPP
-       image = new Pixel[(*rows) * (*cols)];
-#else
-       image = (Pixel *)malloc(sizeof(Pixel)* (*rows) * (*cols));
-#endif
-       if(image) {
-	 // Read the data
-	 fread(image, sizeof(Pixel), (*rows) * (*cols), fp);
-
-	 if(fp != stdin)
-	   fclose(fp);
-
-	 return(image);
-       }
-     }
-
-   }
-   
-   return(NULL);
-     
-} // end read_ppm
-
-// Write the modified image out as a ppm in the correct format to be read by 
-// read_ppm.  xv will read these properly.
-void writePPM(Pixel *image, int rows, int cols, int colors, char *filename)
-{
-  FILE *fp;
-
-  if(filename != NULL && strlen(filename))
-    fp = fopen(filename, "w");
-  else
-    fp = stdout;
-
-  if(fp) {
-    fprintf(fp, "P6\n");
-    fprintf(fp, "%d %d\n%d\n", cols, rows, colors);
-
-    fwrite(image, sizeof(Pixel), rows * cols, fp);
-  }
-
-  fclose(fp);
-
-} // end write_ppm 
-
-
-// Write the modified image out as a pgm in the correct format
-void writePGM(unsigned char *image, long rows, long cols, int intensities, char *filename)
-{
-  FILE *fp;
-
-  if(filename != NULL && strlen(filename))
-    fp = fopen(filename, "w");
-  else
-    fp = stdout;
-
-  if(fp) {
-    fprintf(fp, "P5\n");
-    fprintf(fp, "%ld %ld\n%d\n", cols, rows, intensities);
-
-    fwrite(image, sizeof(unsigned char), rows * cols, fp);
-  }
-
-  if(fp != stdout)
-    fclose(fp);
-} // end write_pgm 
-
-
-// read in intensity values from the pgm file
-unsigned char *readPGM(int *rows, int *cols, int *intensities, char *filename) {
-   char tag[40];
-   unsigned char *image;
-   FILE *fp;
-   int read, num[3], curchar;
-
-   if(filename != NULL && strlen(filename))
-     fp = fopen(filename, "r");
-   else
-     fp = stdin;
-
-   if(fp) {
-     fscanf(fp, "%s\n", tag);
-
-     // Read the "magic number" at the beginning of the ppm
-     if (strncmp(tag, "P5", 40) != 0) {
-       fprintf(stderr, "not a ppm!\n");
-       exit(1);
-     }
-     
-     // Read the rows, columns, and color depth output by cqcam
-     // need to read in three numbers and skip any lines that start with a #
-     read = 0;
-     while(read < 3) {
-       curchar = fgetc(fp);
-       if((char)curchar == '#') { // skip this line
-	 while(fgetc(fp) != '\n')
-	   /* do nothing */;
-       }
-       else {
-	 ungetc(curchar, fp);
-	 fscanf(fp, "%d", &(num[read]));
-	 read++;
-       }
-     }
-     while(fgetc(fp) != '\n')
-       /* pass the last newline character */;
-
-     *cols = num[0];
-     *rows = num[1];
-     *intensities = num[2];
-
-     if(*intensities != 255) {
-       printf("Unable to read this file correctly\n");
-       return(NULL);
-     }
-
-     if(*cols > 0 && *rows > 0) {
-#if USECPP
-       image = new unsigned char[(*rows) * (*cols)];
-#else
-       image = (unsigned char *)malloc(sizeof(unsigned char) * (*rows) * (*cols));
-#endif
-       if(image) {
-	 // Read the data
-	 fread(image, sizeof(unsigned char), (*rows) * (*cols), fp);
-
-	 if(fp != stdin)
-	   fclose(fp);
-
-	 return(image);
-       }
-     }
-
-   }
-   
-   return(NULL);
-     
-} // end read_pgm
 
