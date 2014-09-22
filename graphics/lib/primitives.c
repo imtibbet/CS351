60a4ea3c7ed6303cf1edd7b52b126ec623c47ef7
astimoore <astrid.moore@gmail.com>
2014-09-22 14:44:41 -0400

    Attempt at Line

--- graphics/lib/primitives.c
+++ graphics/lib/primitives.c
@@ -5,6 +5,7 @@
  * The color function implementations
  */
 
+// POINT
 
 /* 
  * Sets the first two values of the vector to x and y. 
@@ -56,3 +57,187 @@ void point_drawf(Point *p, Image *src, FPixel c){
 	image_setf(src, p->val[0], p->val[1], c);
 }
 
+// LINE
+
+/** Initialize a 2D line. **/
+void line_set2D(Line *l, double x0, double y0, double x1, double y1){
+	Point a;
+	Point b;
+	point_set2D(&a, x0, y0);
+	point_set2D(&b, x1, y1);
+
+	l->a = a;
+	l->b = b;
+}
+
+/** Initialze a line to ta and tb. **/
+void line_set(Line *l, Point ta, Point tb){
+	l->a = ta;
+	l->b = tb;
+}
+
+/** Set the z-buffer flag to the given value. **/
+void line_zBuffer(Line *l, int flag){
+	l->zBuffer = flag;
+}
+
+/** Copy the line data structure. **/
+void line_copy(Line *to, Line *from){
+	to->zBuffer = from->zBuffer;
+	point_copy(&(to->a), &(from->a));
+	point_copy(&(to->b), &(from->b));
+}
+
+/** Draw the line into src using Color c. **/
+void line_draw(Line *l, Image *src, Color c){
+	int x0, y0, x1, y1;
+	int x, y, dx, dy, e, i;
+	Point curp;
+
+	x = x0 = l->a[0];
+	y = y0 = l->a[1];
+	x1 = l->b[0];
+	y1 = l->b[1];
+
+	dx = x1 - x0;
+	dy = y1 - y0;
+
+	if(dy<0){
+		x = x0 = l->b[0];
+		y = y0 = l->b[1];
+		x1 = l->a[0];
+		y1 = l->a[1];
+
+		dx = x1 - x0;
+		dy = y1 - y0;
+	}
+	
+	e = 3*dy-2*dx;
+
+	// 1st and 2nd octants (right half)
+	if(dx>0) {
+		// 1st octant
+		if(dx>=dy) {
+			for(i=0; i<=dx; i++){
+				point_set2D(&curp, (double)x, (double)y);
+				point_draw(&curp, src, c);
+				if(e>0){
+					y++;
+					e-=2*dx;
+				}
+				x++;
+				e+=2*dy;
+			}
+		}
+		// 2nd octant
+		else(dy>dx){
+			for(i=0; i<=dy; i++){
+				point_set2D(&curp, (double)x, (double)y);
+				point_draw(&curp, src, c);
+				if(e>0){
+					x++;
+					e-=2*dy;
+				}
+				y++;
+				e+=2*dx;
+			} 
+		}
+	}
+	// 3rd and 4th octants (left half)
+	else if(dx<0){
+		// 4th octant
+		if(dx>=dy) {
+			for(i=0; i<=dx; i++){
+				point_set2D(&curp, (double)x, (double)y);
+				point_draw(&curp, src, c);
+				if(e>0){
+					y++;
+					e-=2*dx;
+				}
+				x--;
+				e+=2*dy;
+			}
+		}
+		// 3rd octant
+		else(dy>dx){
+			for(i=0; i<=dy; i++){
+				point_set2D(&curp, (double)x, (double)y);
+				point_draw(&curp, src, c);
+				if(e>0){
+					x--;
+					e-=2*dy;
+				}
+				y++;
+				e+=2*dx;
+			} 
+		}
+
+	}
+
+}
+
+// CIRCLE
+
+/** Initialize to center tc and radius tr. **/
+void circle_set(Circle *c, Point tc, double tr){}
+
+/** Draw the circle into src using color p. **/
+void circle_draw(Circle *c, Image *src, Color p){}
+
+/** Draw a filled circled into src using color p. **/
+void circle_drawFill(Circle *c, Image *src, Color p){}
+
+// ELLIPSE
+
+/** Initialize an ellipse to location tc and radii ta and tb. **/
+void ellipse_set(Ellipse *e, Point tc, double ta, double tb){}
+
+/** Draw into src using color p. **/
+void ellipse_draw(Ellipse *e, Image *src, Color p){}
+
+/** Draw a filled ellipse into src using color p. **/
+void ellipse_drawFill(Ellipse *e, Image *src, Color p){}
+
+// POLYLINE
+
+/* Returns an allocated Polyline pointer initialized so that 
+* numVertex is 0 and vertex is NULL. */
+Polyline *polyline_create(){
+	Polyline *p;
+	return(p);
+}
+
+/* Returns an allocated Polyline pointer with the vertex list initialized 
+* to the points in vlist. */
+Polyline *polyline_createp(int numV, Point *vlist){
+	Polyline *p;
+	return(p);
+}
+
+/* Frees the internal data and the Polyline pointer. */
+void polyline_free(Polyline *p){}
+
+/* Initializes the pre-existing Polyline to an empty Polyline. */
+void polyline_init(Polyline *p){}
+
+/* Initializes the vertex list to the points in vlist. De-allocates/allocates
+* the vertex list for p, as necessary. */
+void polyline_set(Polyline *p, int numV, Point *vlist){}
+
+/* Frees the internal data for a Polyline, if necessary, and sets numVertex
+* to 0 and vertex to NULL. */
+void polyline_clear(Polyline *p){}
+
+/* Sets the z-buffer flag to the given value. */
+void polyline_zBuffer(Polyline *p, int flag){}
+
+/* De-allocates/allocates space as necessary in the destination Polyline data
+* structure and copies the vertex data from the source polyline (from) to the 
+* destination (to). */
+void polyline_copy (Polyline *to, Polyline *from){}
+
+/* Prints Polyline data to the stream designated by the FILE pointer. */
+void polyline_print(Polyline *p, FILE *fp){}
+
+/* Draw the Polyline using color c. */
+void polyline_draw(Polyline *p, Image *src, Color c){}
