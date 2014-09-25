c9061cf73c72a3fa6c6cb667cf8435aea34c7ac9
astimoore <astrid.moore@gmail.com>
2014-09-25 15:50:39 -0400

    outline for floodfill.c, empty floodfill.h

---
+++ graphics/lib/floodfill.c
@@ -0,0 +1,33 @@
+/*
+Astrid Moore (Bruce Maxwell)
+Fall 2014
+CS351
+
+Flood fills a shape, like a paint can tool.
+*/
+
+#include "graphics.h"
+
+void floodfill(Point *vlist, Image *src, Color c){
+    //make the boundary using a selected Color c
+    
+    
+    //pick an initial point
+    
+    //color the initial point
+    
+    //push the initial point on a stack
+    
+    
+    //while the stack is not empty
+        //pop a Pixel p off the stack
+        //for each neighbor N of P
+            //if N is not already colored
+                //Color N
+                //push N on the stack
+    
+    
+    
+    
+
+}
