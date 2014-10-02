2a9cbfe032f1795a23bdfd00b4bf790731238731
astimoore <astrid.moore@gmail.com>
2014-10-02 15:55:37 -0400

    updating

--- Proj4/lib/primitives.c
+++ Proj4/lib/primitives.c
@@ -1207,7 +1207,7 @@ static Edge *makeEdgeRec( Point start, Point end, Image *src)
 	Edge *edge;
 	float dscan = end.val[1] - start.val[1];
 	float dwidth = end.val[0] - start.val[0];
-	float xAdjust, vyMinusFloor, floorMinusVy;
+	float xAdjust, vyMinusFloor;
 
 	/******
 				 Your code starts here
@@ -1247,21 +1247,16 @@ static Edge *makeEdgeRec( Point start, Point end, Image *src)
 	}
 
 	// Calculate the slope, dxPerScan
-	if(dwidth<epsilon && dwidth>-epsilon){
-		edge->dxPerScan = 0.0;
-	} else {
-		edge->dxPerScan = dscan/dwidth;
-	}
+	edge->dxPerScan = dwidth/dscan;
 	
 	// Calculate xIntersect, adjusting for the fraction of the point in the pixel.
 	// Scanlines go through the middle of pixels
 	// Move the edge to the first scanline it crosses
-	floorMinusVy = ((float)((int)(edge->y0))) - (edge->y0);
 	vyMinusFloor = (edge->y0) - ((float)((int)(edge->y0)));
-	if(0.5 < vyMinusFloor){
-		xAdjust = 1.5 - floorMinusVy;
+	if(0.5 <= vyMinusFloor){
+		xAdjust = 1.5 - vyMinusFloor;
 	} else {
-		xAdjust = 0.5 - floorMinusVy;
+		xAdjust = 0.5 - vyMinusFloor;
 	}
 	edge->xIntersect = edge->x0 + xAdjust*edge->dxPerScan;
 	
@@ -1282,9 +1277,9 @@ static Edge *makeEdgeRec( Point start, Point end, Image *src)
 
 	// check for really bad cases with steep slopes where xIntersect 
 	// has gone beyond the end of the edge
-	if(edge->xIntersect>(edge->y1)){
+	if(edge->dxPerScan>(edge->x1)){
 		printf("bad bad xIntersect has gone beyond the end of the edge\n");
-		//return(NULL);
+		edge->dxPerScan = edge->x1-edge->x0;
 	}
 
 	// return the newly created edge data structure
@@ -1365,7 +1360,7 @@ static void fillScan( int scan, LinkedList *active, Image *src, Color c ) {
 
 		/**** Your code goes here ****/
 		// identify the starting column
-		i=(int)(fmin(p1->xIntersect,p2->xIntersect)+0.5);
+		i=(int)(p1->xIntersect);
 		//printf("i=%d",i);
 		// clip to the left side of the image
 		if(i<0){
@@ -1374,7 +1369,7 @@ static void fillScan( int scan, LinkedList *active, Image *src, Color c ) {
 		}
 		
 		// identify the ending column
-		f=(int)(fmax(p1->xIntersect,p2->xIntersect)+0.5);
+		f=(int)(p2->xIntersect);
 		//printf("f=%d\n",f);
 		
 		// clip to the right side of the image
