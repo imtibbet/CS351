588499d7bc6959fc5ad4c735717f43c4f5421081
astimoore <astrid.moore@gmail.com>
2014-11-08 15:38:27 -0500

    editing fire.c fire-test.c to animate flames

--- dev/src/fire.c
+++ dev/src/fire.c
@@ -28,7 +28,7 @@ static Module * genWood(float x, float y, float z){
 	color_set(&dkBrown1, (float)(72/255.0), (float)(60/255.0), (float)(50/255.0));
 	color_set(&final, 0, 0, 0);
 
-	gen = (int)(rand() % 3);
+	gen = (int)(rand() % 3 + 0.5);
 	if (gen == 0){
 		color_copy(&final, &brown0);
 		color_copy(&outlineC, &dkBrown0);
@@ -52,13 +52,6 @@ static Module * genWood(float x, float y, float z){
 
 	thickness = 10;
 
-	module_scale(outline, 1, 4, 1);
-	module_rotateX(outline, 0, 1);
-	module_translate(outline, x, y, z);
-	module_color(outline, &outlineC);
-	module_cylinder(outline, 20, 0, thickness, 0, 0, 0);
-	module_module(woodpile, outline);
-
 	module_scale(wood, 1, 4, 1);
 	module_rotateX(wood, 0, 1);
 	module_translate(wood, x, y, z);
@@ -66,48 +59,16 @@ static Module * genWood(float x, float y, float z){
 	module_cylinder(wood, 20, 1, thickness, 0, 0, 0);
 	module_module(woodpile, wood);
 
+	module_scale(outline, 1, 4, 1);
+	module_rotateX(outline, 0, 1);
+	module_translate(outline, x, y, z);
+	module_color(outline, &outlineC);
+	module_cylinder(outline, 20, 0, thickness, 0, 0, 0);
+	module_module(woodpile, outline);
+
 	printf("added wood\n");
 
 	return((Module*)woodpile);
-}
-
-static Module* genWoodpile(float x, float y, float z);
-static Module* genWoodpile(float x, float y, float z){
-	Module *wood, *woodpile;
-	woodpile = module_create();
-
-	wood = module_create();
- 	wood = genWood(x, y, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(2+x, y, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(4+x, y, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(x-2, y, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(x-1, y+1.5, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(x+1, y+1.5, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(x+3, y+1.5, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(x+2, y+3, z);
- 	module_module(woodpile, wood);
- 	wood = module_create();
- 	wood = genWood(x, y+3, z);
- 	module_module(woodpile, wood);
-
- 	printf("added woodpile\n");
-
- 	return((Module*)woodpile);
-
 }
 
 /* Creates a flame using a bezier surface*/
@@ -115,11 +76,38 @@ static Module* genFlame(float x, float y, float z, int divisions);
 static Module* genFlame(float x, float y, float z, int divisions){
 	Module *flame;
 	BezierSurface bc;
-	Color red;
+	int gen;
+	Color ltOrange, orange, dkOrange, red, yellow, amber, final;
 	Point p[16];
+	srand(time(NULL));
+	color_set(&ltOrange, 1, (float)(167/255), 0);
+	color_set(&orange, 1, (float)(127/255), 0);
+	color_set(&dkOrange, 1, (float)(103/255), 0);
 	color_set(&red, 1, 0, 0);
-
+	color_set(&yellow, 1, (float)(233/255), 0);
+	color_set(&amber, 1, (float)(191/255), 0);
+	color_set(&final, 0, 0, 0);
 	flame = module_create();
+
+	gen = (int)(rand() % 5 + 0.5);
+	if (gen == 0){
+		color_copy(&final, &ltOrange);		
+	}
+	if (gen == 1){
+		color_copy(&final, &orange);		
+	}
+	if (gen == 2){
+		color_copy(&final, &dkOrange);		
+	}
+	if (gen == 3){
+		color_copy(&final, &red);		
+	}
+	if (gen == 4){
+		color_copy(&final, &yellow);
+	}
+	else {
+		color_copy(&final, &amber);
+	}
 
 	// create a curved surface sitting above the plane
 	point_set3D(&p[0], 0.0, 0.0, 0.0); // first row, constant x, even spacing in z
@@ -140,8 +128,11 @@ static Module* genFlame(float x, float y, float z, int divisions){
 	point_set3D(&p[15], 1.0, 1.0, 1.0);
 	bezierSurface_set(&bc, p);
 
-	module_color(flame, &red);
+	module_scale(flame, (float)(rand()%10), (float)(rand()%30), (float)(rand()%20));
+	module_color(flame, &final);
 	module_bezierSurface(flame, &bc, divisions, 1);
+
+	printf("Creating flame with %d subdivisions\n", divisions);
 
 	return((Module*)flame);
 }
@@ -150,10 +141,8 @@ int main(int argc, char *argv[]) {
 	// initialize fields
 	const int rows = 600*2;
 	const int cols = 660*2;
-	int frame;
-	int divisions = 5;
 	Image *src;
-	Module *woodpile, *fireside, *flame;
+	Module *woodpile, *fireside, *flame[1];
 	Polygon p;
 	View3D view;
 	Matrix vtm, gtm;
@@ -161,23 +150,22 @@ int main(int argc, char *argv[]) {
 	char command[256];
 	float alpha;
 	Color blue, green;
+	int i;
 	color_set(&blue, 0, 0, 1);
 	color_set(&green, 0, 1, 0);
 
-	// grab the command line argument, if one exists
-	if(argc > 1) {
-		int tmp = atoi(argv[1]);
-		if( tmp >= 0 && tmp < 10 )
-			divisions = tmp;
-	}
-	printf("Creating Bezier surface with %d subdivisions\n", divisions);
-
-	// scene setup
-	woodpile = module_create();
 	fireside = module_create();
 
-	woodpile = (Module*)genWoodpile(0, 0, 0);
-	module_module(fireside, woodpile);
+	// grab command line argument to determine viewpoint
+    // and set up the view structure
+    if( argc > 1 ) {
+        alpha = atof( argv[1] );
+        if( alpha < 0.0 || alpha > 1.0 )
+            alpha = 0.0;
+        point_set3D( &(view.vrp), 40*alpha, 140*alpha, -170*alpha - (alpha*170-cos(alpha)*170) );
+    } else {
+		point_set3D( &(view.vrp), 0, 0, -170 );
+    }
 
 	// set up the view
 	vector_set( &(view.vpn), -view.vrp.val[0], -view.vrp.val[1], -view.vrp.val[2] );
@@ -194,38 +182,70 @@ int main(int argc, char *argv[]) {
 	matrix_setView3D( &vtm, &view );
  	matrix_identity( &gtm );
 
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(0, 0, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(2, 0, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(4, 0, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(-2, 0, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(-1, 1.5, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(1, 1.5, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(3, 1.5, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(2, 3, 0);
+ 	// module_module(fireside, woodpile);
+ 	// woodpile = module_create();
+ 	// woodpile = genWood(0, 3, 0);
+ 	// module_module(fireside, woodpile);
+
+ 	// try sizeof(flame[])
+ 	for ( i = 0; i < 1; i++){
+ 		flame[i] = module_create();
+ 		flame[i] = genFlame(0, 0, 0, 5);
+ 		module_module(fireside, flame[i]);
+ 	}
+
 	color_set(&blue, 0, 0, 1);
 
 	// create the image and drawstate
 	src = image_create( rows, cols );
+	image_fillColor(src, blue);
 	ds = drawstate_create();
 	ds->shade = ShadeConstant;
 	printf("created the image and drawstate\n");
 
-	// Create the animation by adjusting the GTM
-	for(frame=0;frame<60;frame++) {
-		char buffer[256];
-
-		matrix_rotateY(&gtm, cos(M_PI/30.0), sin(M_PI/30.0) );
-		module_draw( fireside, &vtm, &gtm, &ds, NULL, src );
-
-		sprintf(buffer, "fire-frame%03d.ppm", frame);
-		image_write(src, buffer);
-		image_reset(src);
-	}
+	// draw scene
+	module_draw(fireside, &vtm, &gtm, ds, NULL, src );
+	printf("drew the scene\n");
 
 	// write out the scene
-	printf("converting to gif...\n");
-	system("convert -delay 1.5 -loop 0 fire-frame*.ppm fire.gif");
-	system("rm fire-frame*.ppm");
+	printf("Writing image\n");
+	image_write( src, "fire.ppm" );
+	sprintf(command, "convert -scale %03dx%03d fire.ppm fire.ppm", cols/2, rows/2);
+	system(command);
 
 	// free the polygon data
 	polygon_clear( &p );
 	printf("polygon freed\n");
 
 	// free the modules
-	module_delete(woodpile);
+	for ( i = 0; i < sizeof(flame); i++){
+		module_delete(flame[i]);
+	}
 	module_delete(fireside);
+
 	printf("module freed\n");
 
 	// free drawstate
