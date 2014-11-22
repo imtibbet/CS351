1b1480e6a684a95facd8007d021318dc7f6a5fa8
imtibbet <tibbian@gmail.com>
2014-11-22 18:03:09 -0500

    lighting shading done, untested
    need to do polygon shade

--- graphics/lib/lighting.c
+++ graphics/lib/lighting.c
@@ -117,8 +117,9 @@ void lighting_add( Lighting *l, Color *c, Vector *dir, Point *pos, float cutoff,
 void lighting_shading( Lighting *l, Vector *N, Vector *V, Point *p, 
 	Color *Cb, Color *Cs, float s, int oneSided, Color *c){
 	int i;
+	float LdotN, HdotNpowS, nLdotD, spotSharp;
 	Color curc = {{0.0, 0.0, 0.0}};
-	Vector L, H;
+	Vector L, nL, H;
 	if(!l || !N || !V || !p || !Cb || !Cs){
 		printf("Null passed to lighting_shading\n");
 		return;
@@ -126,18 +127,37 @@ void lighting_shading( Lighting *l, Vector *N, Vector *V, Point *p,
 	for (i = 0; i < l->nLights; i++)
 	{
 		switch(l->light[i].type){
+
 			case LightAmbient:
-				curc.c[0] += Cb->c[0]*l->light[i].color.c[0];
-				curc.c[1] += Cb->c[1]*l->light[i].color.c[1];
-				curc.c[2] += Cb->c[2]*l->light[i].color.c[2];
+				curc.c[0] += Cb->c[0] * l->light[i].color.c[0];
+				curc.c[1] += Cb->c[1] * l->light[i].color.c[1];
+				curc.c[2] += Cb->c[2] * l->light[i].color.c[2];
 				break;
+
 			case LightDirect:
-				vector_copy(&L, &(l->light[i].direction));
+				vector_set(&L, 	-1 * l->light[i].direction.val[0], 
+								-1 * l->light[i].direction.val[1]
+								-1 * l->light[i].direction.val[2]);
 				vector_set(&H,	0.5*(L.val[0] + V->val[0]), 
 								0.5*(L.val[1] + V->val[1]), 
 								0.5*(L.val[2] + V->val[2]) );
-
+				if(oneSided && vector_dot(&L, N) < 0){
+					N->val[0] *= -1;
+					N->val[1] *= -1;
+					N->val[2] *= -1;
+				}
+				LdotN = vector_dot(&L, N);
+				if(LdotN > 0){
+					HdotNpowS = pow(vector_dot(&H, N), s);
+					curc.c[0] += 	Cb->c[0] * l->light[i].color.c[0] * LdotN + 
+									Cs->c[0] * l->light[i].color.c[0] * HdotNpowS;
+					curc.c[1] += 	Cb->c[1] * l->light[i].color.c[1] * LdotN + 
+									Cs->c[1] * l->light[i].color.c[1] * HdotNpowS;
+					curc.c[2] += 	Cb->c[2] * l->light[i].color.c[2] * LdotN + 
+									Cs->c[2] * l->light[i].color.c[2] * HdotNpowS;
+				}
 				break;
+
 			case LightPoint:
 				vector_set(&L, 	l->light[i].position.val[0] - p->val[0], 
 								l->light[i].position.val[1] - p->val[1], 
@@ -145,21 +165,64 @@ void lighting_shading( Lighting *l, Vector *N, Vector *V, Point *p,
 				vector_set(&H,	0.5*(L.val[0] + V->val[0]), 
 								0.5*(L.val[1] + V->val[1]), 
 								0.5*(L.val[2] + V->val[2]) );
-
+				if(oneSided && vector_dot(&L, N) < 0){
+					N->val[0] *= -1;
+					N->val[1] *= -1;
+					N->val[2] *= -1;
+				}
+				LdotN = vector_dot(&L, N);
+				if(LdotN > 0){
+					HdotNpowS = pow(vector_dot(&H, N), s);
+					curc.c[0] += 	Cb->c[0] * l->light[i].color.c[0] * LdotN + 
+									Cs->c[0] * l->light[i].color.c[0] * HdotNpowS;
+					curc.c[1] += 	Cb->c[1] * l->light[i].color.c[1] * LdotN + 
+									Cs->c[1] * l->light[i].color.c[1] * HdotNpowS;
+					curc.c[2] += 	Cb->c[2] * l->light[i].color.c[2] * LdotN + 
+									Cs->c[2] * l->light[i].color.c[2] * HdotNpowS;
+				}
 				break;
+
 			case LightSpot:
-				vector_set(&L, 	l->light[i].position.val[0] - p->val[0], 
-								l->light[i].position.val[1] - p->val[1], 
-								l->light[i].position.val[2] - p->val[2] );
+				vector_set(&L, 	-1 * (l->light[i].position.val[0] - p->val[0]), 
+								-1 * (l->light[i].position.val[1] - p->val[1]), 
+								-1 * (l->light[i].position.val[2] - p->val[2]) );
 				vector_set(&H,	0.5*(L.val[0] + V->val[0]), 
 								0.5*(L.val[1] + V->val[1]), 
 								0.5*(L.val[2] + V->val[2]) );
-
+				if(oneSided && vector_dot(&L, N) < 0){
+					N->val[0] *= -1;
+					N->val[1] *= -1;
+					N->val[2] *= -1;
+				}
+				LdotN = vector_dot(&L, N);
+				vector_set(&nL,	-1 * L.val[0], // negative L
+								-1 * L.val[1], 
+								-1 * L.val[2] );
+				nLdotD = vector_dot(&nL, &(l->light[i].direction));
+				if(	LdotN > 0 && nLdotD > cos(l->light[i].cutoff)){
+					HdotNpowS = pow(vector_dot(&H, N), s);
+					spotSharp = pow(nLdotD, l->light[i].sharpness);
+					curc.c[0] += 	(Cb->c[0] * l->light[i].color.c[0] * LdotN + 
+									Cs->c[0] * l->light[i].color.c[0] * HdotNpowS) * 
+									spotSharp;
+					curc.c[1] += 	(Cb->c[1] * l->light[i].color.c[1] * LdotN + 
+									Cs->c[1] * l->light[i].color.c[1] * HdotNpowS) * 
+									spotSharp;
+					curc.c[2] += 	(Cb->c[2] * l->light[i].color.c[2] * LdotN + 
+									Cs->c[2] * l->light[i].color.c[2] * HdotNpowS) * 
+									spotSharp;
+				}
 				break;
+				
 			default:
 				break;
 		}
 	}
+
+	// clip colors to that are over-saturated down to one
+	c->val[0] = curc.val[0] > 1.0 ? 1.0 : curc.val[0];
+	c->val[1] = curc.val[1] > 1.0 ? 1.0 : curc.val[1];
+	c->val[2] = curc.val[2] > 1.0 ? 1.0 : curc.val[2];
 }
 
 
