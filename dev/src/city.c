47f81199bc3d5b2885e55d62ee57a3c3a2d31645
astimoore <astrid.moore@gmail.com>
2014-12-06 18:11:09 -0500

    wahoo! city on the rise

--- dev/src/city.c
+++ dev/src/city.c
@@ -18,7 +18,8 @@ int main(int argc, char *argv[]) {
 	Lighting *light;
 	Image *src;
 	int i;
-	Module *b[22];
+	int numBuildings;
+	Module *b[numBuildings];
 	Module *city;
 	Polygon p;
 	View3D view;
@@ -38,16 +39,16 @@ int main(int argc, char *argv[]) {
             alpha = 0.0;
         point_set3D( &(view.vrp), 120*alpha, 80*alpha, -80*alpha - (1.0-cos(alpha)*80) );
     } else {
-		point_set3D( &(view.vrp), 50, 0, -30 );
+		point_set3D( &(view.vrp), 40, 0, -100 );
     }
 	// set up the view
 	vector_set( &(view.vpn), -view.vrp.val[0], -view.vrp.val[1], 
 		-view.vrp.val[2] );
 	vector_set( &(view.vup), 0, 1, 0 );
-	view.d = 10;
+	view.d = 5;
 	view.du = 6;
 	view.f = 1;
-	view.b = 50;
+	view.b = 200;
 	view.screenx = cols;
 	view.screeny = rows;
 
@@ -56,149 +57,129 @@ int main(int argc, char *argv[]) {
 	matrix_setView3D( &vtm, &view );
  	matrix_identity( &gtm );
 
+ 	numBuildings = 22;
+
  	// buildings
  	b[0] = module_create();
- 	module_scale( b[0], 4, 4, 20);
- 	module_rotateX( b[0], 0, 1);
- 	module_translate( b[0], -40, 2.5, 0);
+ 	module_scale( b[0], 4, 20, 4);
+ 	module_translate( b[0], -60, 18, 20);
  	module_color(b[0], &Grey);
  	module_cube(b[0], 1);
  	b[1] = module_create();
- 	module_scale( b[1], 7, 3, 2);
- 	module_rotateX( b[1], 0, 1);
- 	module_translate( b[1], -40, -6.5, 10);
+ 	module_scale( b[1], 7, 2, 3);
+ 	module_translate( b[1], -60, 0, -5);
  	module_color(b[1], &Grey);
  	module_cube(b[1], 1);
-
   	b[2] = module_create();
- 	module_scale( b[2], 5, 5, 15);
- 	module_rotateX( b[2], 0, 1);
- 	module_translate( b[2], -30, 0, 0);
+ 	module_scale( b[2], 5, 15, 5);
+ 	module_translate( b[2], -60, 12.5, 20);
  	module_color( b[2], &Grey);
  	module_cube( b[2], 1 );
+
  	b[3] = module_create();
- 	module_scale( b[3], 7, 5, 10);
- 	module_rotateX( b[3], 0, 1);
- 	module_translate( b[3], -30, -2.5, 10);
+ 	module_scale( b[3], 7, 10, 5);
+ 	module_translate( b[3], -40, 7.5, 5);
  	module_color(b[3], &Grey);
  	module_cube(b[3], 1);
  	b[4] = module_create();
- 	module_scale( b[4], 5, 7, 20);
- 	module_rotateX( b[4], 0, 1);
- 	module_translate( b[4], -30, 2.5, 20);
+ 	module_scale( b[4], 5, 20, 7);
+ 	module_translate( b[4], -40, 16.5, 30);
  	module_color(b[4], &Grey);
  	module_cube(b[4], 1);
 
  	b[5] = module_create();
- 	module_scale( b[5], 5, 7, 27);
- 	module_rotateX( b[5], 0, 1);
- 	module_translate( b[5], -20, 6, 0);
+ 	module_scale( b[5], 5, 25, 7);
+ 	module_translate( b[5], -20, 21, 15);
  	module_color(b[5], &Grey);
  	module_cube(b[5], 1); 
  	b[6] = module_create();
- 	module_scale( b[6], 5, 7, 20);
- 	module_rotateX( b[6], 0, 1);
- 	module_translate( b[6], -20, 2.5, 10);
+ 	module_scale( b[6], 5, 20, 7);
+ 	module_translate( b[6], -20, 16, -5);
  	module_color(b[6], &Grey);
  	module_cube(b[6], 1);
  	b[7] = module_create();
- 	module_scale( b[7], 5, 7, 30);
- 	module_rotateX( b[7], 0, 1);
- 	module_translate( b[7], -20, 7.5, 20);
+ 	module_scale( b[7], 5, 30, 7);
+ 	module_translate( b[7], -20, 26.5, 30);
  	module_color(b[7], &Grey);
  	module_cube(b[7], 1);
 
   	b[8] = module_create();
- 	module_scale( b[8], 5, 7, 30);
- 	module_rotateX( b[8], 0, 1);
- 	module_translate( b[8], -10, 7.5, 0);
+ 	module_scale( b[8], 5, 30, 7);
+ 	module_translate( b[8], 0, 26.5, 15);
  	module_color(b[8], &Grey);
  	module_cube(b[8], 1);
  	b[9] = module_create();
- 	module_scale( b[9], 2, 2, 3);
- 	module_rotateX( b[9], 0, 1);
- 	module_translate( b[9], -10, -6, 10);
+ 	module_scale( b[9], 2, 3, 2);
+ 	module_translate( b[9], 0, 0, -5);
  	module_color(b[9], &Grey);
  	module_cube(b[9], 1);
  	b[10] = module_create();
- 	module_scale( b[10], 2, 2, 3);
- 	module_rotateX( b[10], 0, 1);
- 	module_translate( b[10], -10, -6, 15);
+ 	module_scale( b[10], 2, 3, 2);
+ 	module_translate( b[10], 0, 0, 0);
  	module_color(b[10], &Grey);
  	module_cube(b[10], 1);
  	b[11] = module_create();
- 	module_scale( b[11], 4, 4, 20);
- 	module_rotateX( b[11], 0, 1);
- 	module_translate( b[11], -10, 2.5, 20);
+ 	module_scale( b[11], 4, 20, 4);
+ 	module_translate( b[11], 0, 18, 30);
  	module_color(b[11], &Grey);
  	module_cube(b[11], 1);
 
  	b[12] = module_create();
- 	module_scale( b[12], 5, 6, 15);
- 	module_rotateX( b[12], 0, 1);
- 	module_translate( b[12], 0, 0, 12.5);
+ 	module_scale( b[12], 5, 15, 6);
+ 	module_translate( b[12], 20, 12, 30);
  	module_color(b[12], &Grey);
  	module_cube(b[12], 1);
  	b[13] = module_create();
- 	module_scale( b[13], 5, 5, 15);
- 	module_rotateX( b[13], 0, 1);
- 	module_translate( b[13], 0, 0, 0);
+ 	module_scale( b[13], 5, 15, 5);
+ 	module_translate( b[13], 20, 12.5, 20);
  	module_color( b[13], &Grey);
  	module_cube( b[13], 1);
   	b[14] = module_create();
- 	module_scale( b[14], 7, 3, 2);
- 	module_rotateX( b[14], 0, 1);
- 	module_translate( b[14], 0, -6.5, 20);
+ 	module_scale( b[14], 7, 2, 3);
+ 	module_translate( b[14], 20, 0, 10);
  	module_color(b[14], &Grey);
  	module_cube(b[14], 1);
 
  	b[15] = module_create();
- 	module_scale( b[15], 7, 3, 2);
- 	module_rotateX( b[15], 0, 1);
- 	module_translate( b[15], 10, -6.5, 0);
+ 	module_scale( b[15], 7, 2, 3);
+ 	module_translate( b[15], 40, 0, 0);
  	module_color(b[15], &Grey);
  	module_cube(b[15], 1);
  	b[16] = module_create();
- 	module_scale( b[16], 4, 6, 3);
- 	module_rotateX( b[16], 0, 1);
- 	module_translate( b[16], 10, -6, 10);
+ 	module_scale( b[16], 4, 5, 6);
+ 	module_translate( b[16], 40, 2, 10);
  	module_color(b[16], &Grey);
  	module_cube(b[16], 1);
  	b[17] = module_create();
- 	module_scale( b[17], 7, 5, 10);
- 	module_rotateX( b[17], 0, 1);
- 	module_translate( b[17], 10, -2.5, 20);
+ 	module_scale( b[17], 7, 10, 5);
+ 	module_translate( b[17], 40, 7.5, 25);
  	module_color(b[17], &Grey);
  	module_cube(b[17], 1);
 
   	b[18] = module_create();
- 	module_scale( b[18], 2, 2, 5);
- 	module_rotateX( b[18], 0, 1);
- 	module_translate( b[18], 20, -5, 0);
+ 	module_scale( b[18], 2, 5, 2);
+ 	module_translate( b[18], 60, 4, 30);
  	module_color(b[18], &Grey);
  	module_cube(b[18], 1);
  	b[19] = module_create();
- 	module_scale( b[19], 8, 3, 4);
- 	module_rotateX( b[19], 0, 1);
- 	module_translate( b[19], 20, -5.5, 10);
+ 	module_scale( b[19], 8, 4, 3);
+ 	module_translate( b[19], 60, 2.5, 15);
  	module_color(b[19], &Grey);
  	module_cube(b[19], 1);
 
  	b[20] = module_create();
- 	module_scale( b[20], 7, 3, 2);
- 	module_rotateX( b[20], 0, 1);
- 	module_translate( b[20], 30, -6.5, 0);
+ 	module_scale( b[20], 7, 2, 3);
+ 	module_translate( b[20], 70, 0, 0);
  	module_color(b[20], &Grey);
  	module_cube(b[20], 1);
  	b[21] = module_create();
- 	module_scale( b[21], 7, 5, 10);
- 	module_rotateX( b[21], 0, 1);
- 	module_translate( b[21], 30, -2.5, 10);
+ 	module_scale( b[21], 7, 10, 5);
+ 	module_translate( b[21], 70, 7.5, 20);
  	module_color(b[21], &Grey);
  	module_cube(b[21], 1);
 
  	// organize into a city
- 	for ( i = 0; i < 22; i++){
+ 	for ( i = 0; i < numBuildings; i++){
  		module_module(city, b[i]);
 	}
 
@@ -209,38 +190,6 @@ int main(int argc, char *argv[]) {
 
 	point_copy(&(ds->viewer), &(view.vrp));
 	ds->shade = ShadeGouraud;
-
-	//lighting_shading( Lighting *l, Vector *N, Vector *V, Point *p, 
-	//Color *Cb, Color *Cs, float s, int oneSided, Color *c)
-
-	//A reasonable way to structure the code is as follows. (Bruce Maxwell)
-
-	// Before the loop over lights, normalize N and V
-
-	// Within the loop
-
-	//  Within the Point Lighting case (and Spot, and Direct)
-	//    Calculate L
-	//    Normalize L
-
-	//    Calculate theta = L*N
-	//    if the polygon is one-sided and L*N is negative
-	//      continue
-
-	//    Calculate sigma = V*N
-	//    if (theta < 0 and  sigma > 0) or (theta > 0 and sigma < 0)
-	//      continue
-
-	//    Calculate H
-	//    Calculate beta = H*N
-	//    if L*N < 0
-	//      negate theta
-	//      negate beta
-
-	//    calculate shading
-
-	// light = lighting_create();
-	// lighting_add(light, LightAmbient, &White, NULL, &temp, 0.0, 0.0 );
 
 	light = lighting_create();
 	light->light[0].type = LightPoint;
@@ -268,10 +217,7 @@ int main(int argc, char *argv[]) {
 	printf("polygon freed\n");
 
 	// free the modules
-	for ( i = 0; i < 22; i++){
-		module_delete( b[i] );
-	}
-
+	module_delete( city );
 	printf("module freed\n");
 
 	// free drawstate
